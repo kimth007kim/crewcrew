bd5a64740cdfff5eafda507a28e0ee0a26af51ff
kimth007kim <kimth007kim@naver.com>
2021-12-27 22:22:28 +0900

    [Feat] 백엔드 개인 폴더 생성

---
+++ back/sky/crewcrew/src/test/java/matchTeam/crewcrew/CrewcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package matchTeam.crewcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewcrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
