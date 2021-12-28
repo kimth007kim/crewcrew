e24dd9b98a36c703f71c03a7d8cce7b688b82f5c
kimth007kim <kimth007kim@naver.com>
2021-12-28 19:58:23 +0900

    [Chore] 크루크루어플리케이션 스펠링 변경

--- back/crewcrew/src/main/java/matchTeam/crewcrew/crewcrewApplication.java
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/crewcrewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class crewcrewApplication {
+public class CrewcrewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(crewcrewApplication.class, args);
+		SpringApplication.run(CrewcrewApplication.class, args);
 	}
 
 }
