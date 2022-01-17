5557d60d1fd6f256fb532d464c8ce234aeb01def
Slowth-KIM <rt.slowth@gmail.com>
2022-01-17 20:09:49 +0900

    [Feat] Board 엔티티 생성

---
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/entity/board/Board.java
@@ -0,0 +1,22 @@
+package matchTeam.crewcrew.entity.board;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import matchTeam.crewcrew.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@RequiredArgsConstructor
+@Table(name = "board")
+public class Board {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_seq")
+    private Long boardSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "uid")
+    private User user;
+}
