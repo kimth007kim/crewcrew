d74311a854bfd1dd8900c643868f8c6586373a66
Slowth-KIM <rt.slowth@gmail.com>
2022-01-22 18:35:41 +0900

    [Feat] Mybatis 삭제 - JPA로 충분히 가능할 법함

--- back/crewcrew/src/main/java/matchTeam/crewcrew/repository/board/BoardRepository.java
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/repository/board/BoardRepository.java
@@ -1,11 +1,9 @@
 package matchTeam.crewcrew.repository.board;
 
 import matchTeam.crewcrew.entity.board.Board;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Mapper
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
