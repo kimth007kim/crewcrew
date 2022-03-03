eeeaa9af098092a60fa78a3aef1e2b7e2ab42823
digitalworks_hgpark <phk9436phk@gmail.com>
2022-03-03 17:56:26 +0900

    [Publish] 게시글 리스트 퍼블리싱
    - 갤럭시 폴드 레이아웃 아직 다시 맞춰야 함

---
+++ frontend/publish/postlist/PostList.js
@@ -0,0 +1,115 @@
+window.addEventListener("DOMContentLoaded", function(){
+
+    const body = document.querySelector("body");
+    const buttonTop = document.querySelector(".ScrollTop");
+    const buttonBottom = document.querySelector(".ScrollBottom");
+    if(window.scrollY <= 100){
+        buttonTop.classList.add("Disable");
+    } else {
+        buttonTop.classList.remove("Disable");
+    }
+    if(window.scrollY >= body.offsetHeight - screen.availHeight){
+        buttonBottom.classList.add("Disable");
+    } else {
+        buttonBottom.classList.remove("Disable");
+    }
+
+    
+    window.addEventListener("scroll", function(){ //스크롤 버튼 이벤트
+        let scrollTop = window.scrollY;
+        if(scrollTop <= 100){
+            buttonTop.classList.add("Disable");
+        } else {
+            buttonTop.classList.remove("Disable");
+        }
+        if(scrollTop >= body.offsetHeight - screen.availHeight){
+            buttonBottom.classList.add("Disable");
+        } else {
+            buttonBottom.classList.remove("Disable");
+        }
+    });
+
+    buttonTop.addEventListener("click", function(){ //스크롤 버튼 이벤트
+        if(!this.classList.contains("Disable")){
+            window.scrollTo(0, 0);
+        }
+    });
+
+    buttonBottom.addEventListener("click", function(){ //스크롤 버튼 이벤트
+        if(!this.classList.contains("Disable")){
+            window.scrollTo(0, body.offsetHeight);
+        }
+    });
+
+    const FilterWrap = document.querySelector(".FilterWrapper");
+    let FilterOffset = window.pageYOffset + FilterWrap.getBoundingClientRect().top;
+    window.addEventListener("resize", function(){
+        FilterOffset = window.pageYOffset + FilterWrap.getBoundingClientRect().top;
+    });
+    window.addEventListener("scroll", function(){ //스크롤 시 필터 sticky기능
+        if(window.innerWidth>=768){
+            if(window.scrollY >= FilterOffset){
+                FilterWrap.classList.add("Fixed");
+            } else {
+                FilterWrap.classList.remove("Fixed");
+            }
+        } else {
+            if(window.scrollY >= FilterOffset - 60){
+                FilterWrap.classList.add("Fixed");
+            } else {
+                FilterWrap.classList.remove("Fixed");
+            }
+        }
+        
+    });
+
+    const filterButton = document.querySelector(".FilterButton");
+    const filterDown = document.querySelector(".FliterListWrapper");
+    const filterArrow = document.querySelector(".FilterArrow");
+    let isClicked = true;
+    filterButton.addEventListener("click", function(){ //모바일 필터 펼치기
+        if(isClicked){
+            filterDown.classList.add("On");
+            filterArrow.classList.add("On");
+        } else {
+            filterDown.classList.remove("On");
+            filterArrow.classList.remove("On");
+        }
+        isClicked = !isClicked;
+    });
+
+    const pagination = document.querySelector(".PaginationWrapper");
+    function paginationAppend(){ //브라우저 사이즈에 따른 페이지네이션 생성
+        let div, divLength, i, pageNum, paginationChild;
+        i = 0;
+
+        while(pagination.hasChildNodes()){ //페이지네이션 초기화
+            pagination.removeChild(pagination.firstChild);
+        }
+
+        if(window.innerWidth>=768){ //페이지네이션 갯수 설정
+            divLength = 14;
+        } else {
+            divLength = 9;
+        }
+
+        for(i; i<divLength; i++){ //페이지네이션 생성
+            div = document.createElement("div");
+            if(i!=0 && i!=1 && i!=divLength - 2 && i!=divLength - 1){
+                pageNum = document.createTextNode(i - 1);
+                div.appendChild(pageNum);
+            }
+            pagination.appendChild(div);
+        }
+
+        paginationChild = pagination.children; //페이지네이션 Arrow 지정
+        paginationChild[0].classList.add("Prev2");
+        paginationChild[1].classList.add("Prev");
+        paginationChild[2].classList.add("On");
+        paginationChild[paginationChild.length - 2].classList.add("Next");
+        paginationChild[paginationChild.length - 1].classList.add("Next2");
+    }
+
+    paginationAppend();
+    window.addEventListener("resize", paginationAppend);
+});
