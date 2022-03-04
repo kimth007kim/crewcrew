d8c77f91d958e6bee81d74ab8e39811e27776c8e
han_kyeul <phk9436phk@gmail.com>
2022-03-04 09:00:05 +0900

    [Publish] 게시글 리스트 수정
    - Fold사이즈 레이아웃 맞춤

--- frontend/publish/postlist/PostList.js
+++ frontend/publish/postlist/PostList.js
@@ -42,19 +42,17 @@ window.addEventListener("DOMContentLoaded", function(){
     });
 
     const FilterWrap = document.querySelector(".FilterWrapper");
-    let FilterOffset = window.pageYOffset + FilterWrap.getBoundingClientRect().top;
-    window.addEventListener("resize", function(){
-        FilterOffset = window.pageYOffset + FilterWrap.getBoundingClientRect().top;
-    });
+
     window.addEventListener("scroll", function(){ //스크롤 시 필터 sticky기능
+
         if(window.innerWidth>=768){
-            if(window.scrollY >= FilterOffset){
+            if(window.scrollY >= 358){
                 FilterWrap.classList.add("Fixed");
             } else {
                 FilterWrap.classList.remove("Fixed");
             }
         } else {
-            if(window.scrollY >= FilterOffset - 60){
+            if(window.scrollY >= 240){
                 FilterWrap.classList.add("Fixed");
             } else {
                 FilterWrap.classList.remove("Fixed");
@@ -76,6 +74,23 @@ window.addEventListener("DOMContentLoaded", function(){
             filterArrow.classList.remove("On");
         }
         isClicked = !isClicked;
+    });
+
+    const filterAll = document.querySelector("#CategoryAll");
+    const filterCat = document.querySelectorAll(".FilterCategory");
+    filterAll.addEventListener("click", function(i){ //전체 체크 시 카테고리 체크 해제
+        filterCat.forEach(function(e){
+            if(i.target.checked){
+                e.checked = false;
+            }
+        });
+    });
+    filterCat.forEach(function(e){
+        e.addEventListener("click", function(){ //카테고리 체크 시 전체 체크 해제
+            if(this.checked){
+                filterAll.checked = false;
+            }
+        });
     });
 
     const pagination = document.querySelector(".PaginationWrapper");
