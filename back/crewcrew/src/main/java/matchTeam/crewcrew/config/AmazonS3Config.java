04081da4cab203731e3e8f55c9386bcfbeff576c
kimth007kim <kimth007kim@naver.com>
2022-03-11 21:38:02 +0900

    [Fix] application.yml 수정

--- back/crewcrew/src/main/java/matchTeam/crewcrew/config/AmazonS3Config.java
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/config/AmazonS3Config.java
@@ -11,9 +11,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmazonS3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
