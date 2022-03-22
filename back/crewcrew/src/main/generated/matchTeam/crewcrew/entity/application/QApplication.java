4bea9068424520595234844629f089964a2b6424
Slowth-KIM <rt.slowth@gmail.com>
2022-03-22 20:45:15 +0900

    [Feat] 지원하기 Entity 생성

---
+++ back/crewcrew/src/main/generated/matchTeam/crewcrew/entity/application/QApplication.java
@@ -0,0 +1,58 @@
+package matchTeam.crewcrew.entity.application;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QApplication is a Querydsl query type for Application
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QApplication extends EntityPathBase<Application> {
+
+    private static final long serialVersionUID = -950262393L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QApplication application = new QApplication("application");
+
+    public final BooleanPath acceptance = createBoolean("acceptance");
+
+    public final matchTeam.crewcrew.entity.board.QBoard board;
+
+    public final StringPath commetary = createString("commetary");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final matchTeam.crewcrew.entity.user.QUser uid;
+
+    public QApplication(String variable) {
+        this(Application.class, forVariable(variable), INITS);
+    }
+
+    public QApplication(Path<? extends Application> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QApplication(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QApplication(PathMetadata metadata, PathInits inits) {
+        this(Application.class, metadata, inits);
+    }
+
+    public QApplication(Class<? extends Application> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.board = inits.isInitialized("board") ? new matchTeam.crewcrew.entity.board.QBoard(forProperty("board"), inits.get("board")) : null;
+        this.uid = inits.isInitialized("uid") ? new matchTeam.crewcrew.entity.user.QUser(forProperty("uid")) : null;
+    }
+
+}
+
---
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/service/application/ApplicationService.java
@@ -0,0 +1,12 @@
+package matchTeam.crewcrew.service.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class ApplicationService {
+
+}
