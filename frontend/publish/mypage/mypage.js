40cec669d75d6a6ac27995d85ded722c21aa7593
digitalworks_hgpark <phk9436phk@gmail.com>
2022-04-14 12:43:30 +0900

    [Publish] 마이페이지 기능수정

--- frontend/publish/mypage/mypage.js
+++ frontend/publish/mypage/mypage.js
@@ -2,9 +2,8 @@ window.addEventListener("DOMContentLoaded", function(){
 
     const InfoBody = document.querySelector(".myInfoBody");
     const InfoButton = document.querySelector(".ButtonFull3");
-    let InfoBodyHeight = InfoBody.clientHeight;
-    let isOn = true;
-    InfoBody.style.height = 0;
+    let InfoBodyHeight, isOn;
+    
     InfoButton.addEventListener("click", function(){
         if(window.innerWidth <= 820){
             isOn ? 
@@ -15,7 +14,17 @@ window.addEventListener("DOMContentLoaded", function(){
             isOn = !isOn;
         }
     });
-    if(window.innerWidth <= 820){
-        InfoButton.innerText = "프로필 관리하기";
+    function mobileView(){
+        InfoBody.style.height = 'auto';
+        InfoBodyHeight = InfoBody.clientHeight;
+        isOn = true;
+        window.innerWidth <= 820 ?
+        (InfoButton.innerText = "프로필 관리하기",
+        InfoBody.style.height = 0) :
+        (InfoButton.innerText = "저장",
+        InfoBody.style.height = InfoBodyHeight);
     }
+    mobileView();
+    window.addEventListener("resize", mobileView);
+    
 });
