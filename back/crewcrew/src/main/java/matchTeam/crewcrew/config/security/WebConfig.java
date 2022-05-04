168aebe1889dd40f1e5e19a570fb835e41488506
kimth007kim <kimth007kim@naver.com>
2022-05-04 17:18:52 +0900

    [Fix] cors 재설정 - www 제거 후 성공

--- back/crewcrew/src/main/java/matchTeam/crewcrew/config/security/WebConfig.java
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/config/security/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://www.crewcrew.org")
+                .allowedOrigins("http://localhost", "http://crewcrew.org")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
                 .allowedHeaders("*")
                 .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true);;
+                .allowCredentials(true);
     }
 }
