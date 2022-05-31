7dd47bbdbc0e5ebf29f3f8f8d7c9311a4cd7ee0a
kimth007kim <kimth007kim@naver.com>
2022-05-31 12:37:56 +0900

    [Fix] logOut -> logout 스펠링 변경

--- back/crewcrew/src/main/java/matchTeam/crewcrew/controller/api/v1/user/AuthController.java
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/controller/api/v1/user/AuthController.java
@@ -498,7 +498,7 @@ public class AuthController {
 
     })
     @ApiOperation(value ="로그아웃" )
-    @DeleteMapping("/logOut")
+    @DeleteMapping("/logout")
     public ResponseEntity<Object> logOut(HttpServletRequest request, HttpServletResponse response) {
         cookieService.logOut(request,response);
             return ResponseHandler.generateResponse("로그아웃 성공", HttpStatus.OK, null );
