f51d16990ef2eca103c13f8f1a377d2604cbc1d2
OH-JOO-YEONG <brb1111@naver.com>
2022-06-08 23:09:02 +0900

    [Feat] 마이페이지 내 크루현황 관리 API 연결

--- frontend/src/components/mypage/CrewBox/index.jsx
+++ frontend/src/components/mypage/CrewBox/index.jsx
@@ -1,17 +1,168 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import BoxCard from './BoxCard';
+import { Cookies } from 'react-cookie';
+import fetcher from '@/utils/fetcher';
+import useSWR from 'swr';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 function CrewBox() {
+  const cookies = new Cookies();
+
+  const [crewRequest, setCrewRequest] = useState(null);
+  const [crewRecruit, setCrewRecruit] = useState(null);
+  const [crewArrive, setCrewArrive] = useState(null);
+  const [crewActivity, setCrewActivity] = useState(null);
+
+  const apiApplication = useCallback(async () => {
+    try {
+      const { data } = await axios.get('/application', {
+        withCredentials: true,
+        headers: {
+          'X-AUTH-TOKEN': cookies.get('X-AUTH-TOKEN'),
+        },
+      });
+      switch (data.status) {
+        case 200:
+          setCrewRequest({ ...data.data });
+          break;
+
+        default:
+          break;
+      }
+    } catch (error) {
+      console.dir(error);
+    }
+  }, []);
+
+  const apiRecruit = useCallback(async () => {
+    try {
+      const { data } = await axios.get('/application/recruiting', {
+        withCredentials: true,
+        headers: {
+          'X-AUTH-TOKEN': cookies.get('X-AUTH-TOKEN'),
+        },
+      });
+      switch (data.status) {
+        case 200:
+          setCrewRecruit({ ...data.data });
+          break;
+
+        default:
+          break;
+      }
+    } catch (error) {
+      console.dir(error);
+    }
+  }, []);
+
+  const apiArrived = useCallback(async () => {
+    try {
+      const { data } = await axios.get('/application/arrived', {
+        withCredentials: true,
+        headers: {
+          'X-AUTH-TOKEN': cookies.get('X-AUTH-TOKEN'),
+        },
+      });
+      switch (data.status) {
+        case 200:
+          setCrewArrive({ ...data.data });
+          break;
+
+        default:
+          break;
+      }
+    } catch (error) {
+      console.dir(error);
+    }
+  }, []);
+
+  const apiActivity = useCallback(async () => {
+    try {
+      let context = {};
+      const { data: crewData } = await axios.get('/application/myCrew', {
+        withCredentials: true,
+        headers: {
+          'X-AUTH-TOKEN': cookies.get('X-AUTH-TOKEN'),
+        },
+      });
+      switch (crewData.status) {
+        case 200:
+          context = { ...crewData.data };
+          break;
+
+        default:
+          console.dir(crewData.message);
+          break;
+      }
+      const { data: participateData } = await axios.get('/application/participated', {
+        withCredentials: true,
+        headers: {
+          'X-AUTH-TOKEN': cookies.get('X-AUTH-TOKEN'),
+        },
+      });
+      switch (participateData.status) {
+        case 200:
+          setCrewActivity({ ...context, ...participateData.data });
+          break;
+
+        default:
+          console.dir(participateData.message);
+
+          break;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const apiCollect = async () => {
+      try {
+        await apiApplication();
+        await apiRecruit();
+        await apiArrived();
+        await apiActivity();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    apiCollect();
+  }, []);
+
   return (
     <Container>
       <h3>내 크루 현황 관리</h3>
       <p>나와 관련된 크루들의 현황을 이곳에서 관리하세요!</p>
       <CrewBoxWrap>
-        <BoxCard />
-        <BoxCard />
-        <BoxCard />
-        <BoxCard />
+        <BoxCard
+          title="내가 참여요청한 크루"
+          total={crewRequest ? crewRequest.totalApplyCount : 0}
+          count_one={crewRequest ? crewRequest.applyToStudyCount : 0}
+          count_two={crewRequest ? crewRequest.applyToHobbyCount : 0}
+        />
+        <BoxCard
+          title="내가 모집중인 크루"
+          total={crewRecruit ? crewRecruit.totalApplyCount : 0}
+          count_one={crewRecruit ? crewRecruit.applyToStudyCount : 0}
+          count_two={crewRecruit ? crewRecruit.applyToHobbyCount : 0}
+        />
+        <BoxCard
+          title="내게 도착한 참여요청"
+          total={crewArrive ? crewArrive.totalApplyCount : 0}
+          count_one={crewArrive ? crewArrive.applyToStudyCount : 0}
+          count_two={crewArrive ? crewArrive.applyToHobbyCount : 0}
+        />
+        <BoxCard
+          title="나의 활동 크루"
+          small_title1="내 마감글"
+          small_title2="참여중 글"
+          total={crewActivity ? crewActivity.myCrewCount + crewActivity.participatedCount : 0}
+          count_one={crewActivity ? crewActivity.myCrewCount : 0}
+          count_two={crewActivity ? crewActivity.participatedCount : 0}
+          deactive={true}
+        />
       </CrewBoxWrap>
     </Container>
   );
