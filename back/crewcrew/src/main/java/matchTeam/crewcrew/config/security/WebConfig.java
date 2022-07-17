67ab6b98920af314f51dbb9de17d2dc70f7a16b1
kimth007kim <kimth007kim@naver.com>
2022-07-17 18:18:06 +0900

    [Fix] CORS URL 추가

--- back/crewcrew/src/main/java/matchTeam/crewcrew/config/security/WebConfig.java
+++ back/crewcrew/src/main/java/matchTeam/crewcrew/config/security/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://crewcrew.org","https://crewcrew.org","https://www.crewcrew.org")
+                .allowedOrigins("http://localhost:3000", "http://crewcrew.org","https://crewcrew.org","https://www.crewcrew.org","http://175.193.24.44:3000")
                 .allowedMethods("POST", "GET","PATCH", "PUT", "OPTIONS", "DELETE", "HEAD")
                 .allowedHeaders("*")
                 .exposedHeaders("X-AUTH-TOKEN","refresh-Token")
